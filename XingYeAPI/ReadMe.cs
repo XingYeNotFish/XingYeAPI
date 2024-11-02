f704b5cb059ae731022421fd2be5d258af4aa958
XingYeFish <154997195+XingYeNotFish@users.noreply.github.com>
2024-11-03 01:35:13 +0800

    上传

---
+++ XingYeAPI/ReadMe.cs
@@ -0,0 +1,10 @@
+﻿namespace XingYeAPI
+{
+    public class ReadMe
+    {
+        // 好吧这里是ReadMe.cs文件，里面什么都没有
+        // 这是我第一次写API,可能会有些不熟练,还请见谅
+        // 顺带一提,我不会用Markdown来写ReadMe :(
+        // 总之感谢你使用XingYeAPI,如果有任何建议或者问题，欢迎联系我 :)
+    }
+}

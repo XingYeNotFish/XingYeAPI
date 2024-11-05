815cd53d95b561f199f7230af74396014d3ba609
XingYeFish <154997195+XingYeNotFish@users.noreply.github.com>
2024-11-06 00:23:45 +0800

    Log已推出!

--- XingYeAPI/Log/PlayerCLog.cs
+++ XingYeAPI/Log/PlayerCLog.cs
@@ -6,14 +6,55 @@ namespace XingYeAPI.Log
     public static class PlayerCLog
     {
         /// <summary>
-        /// 给玩家发送一个带等级的Log信息(待完工中...)
+        /// 给玩家发送一个带等级的Log信息
         /// </summary>
         /// <param name="player"></param>
         /// <param name="message"></param>
-        /// <param name="logLevelType"></param>
-        public static void SendLog(this Player player, string message, XingYeLogLevelType logLevelType = XingYeLogLevelType.Info)
+        /// <param name="LevelType"></param>
+        public static void SendLog(this Player player, string message, XingYeLogLevelType LevelType = XingYeLogLevelType.Info)
         {
-            // 待完工中...
+            string log = "";
+            string color = Color(LevelType);
+            switch (LevelType)
+            {
+                case XingYeLogLevelType.Debug:
+                    log = $"<color={color}>[调试]</color> {message}";
+                    break;
+                case XingYeLogLevelType.Info:
+                    log = $"<color={color}>[信息]</color> {message}";
+                    break;
+                case XingYeLogLevelType.Warning:
+                    log = $"<color={color}>[警告]</color> {message}";
+                    break;
+                case XingYeLogLevelType.Error:
+                    log = $"<color={color}>[错误]</color> {message}";
+                    break;
+                case XingYeLogLevelType.Fatal:
+                    log = $"<color={color}>[致命错误]</color> {message}";
+                    break;
+                default:
+                    break;
+            }
+            player.SendConsoleMessage(log,color);
+        }
+
+        private static string Color(XingYeLogLevelType LevelType)
+        {
+            switch (LevelType)
+            {
+                case XingYeLogLevelType.Debug:
+                    return "#00FF00";
+                case XingYeLogLevelType.Info:
+                    return "#00FFFF";
+                case XingYeLogLevelType.Warning:
+                    return "#FFA500";
+                case XingYeLogLevelType.Error:
+                    return "#FF0000";
+                case XingYeLogLevelType.Fatal:
+                    return "#A020F0";
+                default:
+                    return "";
+            }
         }
     }
 }
--- XingYeAPI/Log/ServerCLog.cs
+++ XingYeAPI/Log/ServerCLog.cs
@@ -5,13 +5,32 @@ namespace XingYeAPI.Log
     public static class ServerCLog
     {
         /// <summary>
-        /// 向服务器后台发送一个带等级的Log信息(待完工中...)
+        /// 向服务器后台发送一个带等级的Log信息
         /// </summary>
         /// <param name="message"></param>
-        /// <param name="logLevelType"></param>
-        public static void SendLog(string message, XingYeLogLevelType logLevelType = XingYeLogLevelType.Info)
+        /// <param name="LevelType"></param>
+        public static void SendLog(string message, XingYeLogLevelType LevelType = XingYeLogLevelType.Info)
         {
-            // 待完工中...
+            switch (LevelType)
+            {
+                case XingYeLogLevelType.Debug:
+                    ServerConsole.AddLog(message, System.ConsoleColor.Green);
+                    break;
+                case XingYeLogLevelType.Info:
+                    ServerConsole.AddLog(message, System.ConsoleColor.Cyan);
+                    break;
+                case XingYeLogLevelType.Warning:
+                    ServerConsole.AddLog(message, System.ConsoleColor.DarkYellow);
+                    break;
+                case XingYeLogLevelType.Error:
+                    ServerConsole.AddLog(message, System.ConsoleColor.Red);
+                    break;
+                case XingYeLogLevelType.Fatal:
+                    ServerConsole.AddLog(message, System.ConsoleColor.DarkRed);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
